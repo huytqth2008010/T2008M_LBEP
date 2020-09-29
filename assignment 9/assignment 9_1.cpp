47858d0c36a5d450a4a003ce354255d312f7d27c
huytqth2008010 <70880121+huytqth2008010@users.noreply.github.com>
2020-09-29 22:25:27 +0700

    Update assignment 9_1.cpp

--- assignment 9/assignment 9_1.cpp
+++ assignment 9/assignment 9_1.cpp
@@ -11,14 +11,14 @@ main(){
 	for(int i=0;i<n;i++){
 		if((s[i]=='a')||(s[i]=='e')||(s[i]=='u')||(s[i]=='o')||(s[i]=='i')){ // Tim Nguyen Am
 			d1++;
-		}if(s[i]>='0'&&s[i]<='9'){  //Tim Ky Tu So
-			d2++;
-		}if((s[i]!='a')&&(s[i]!='e')&&(s[i]!='u')&&(s[i]!='o')&&(s[i]!='i')&&(s[i]>'a')&&(s[i]<'z')){ // Tim Phu Am
-			d3++;
+// 		}if(s[i]>='0'&&s[i]<='9'){  //Tim Ky Tu So
+// 			d2++;
+// 		}if((s[i]!='a')&&(s[i]!='e')&&(s[i]!='u')&&(s[i]!='o')&&(s[i]!='i')&&(s[i]>'a')&&(s[i]<'z')){ // Tim Phu Am
+// 			d3++;
 		}
 	}
 	printf("\nSo nguyen am la : %d",d1);
 //	printf("\nSo phu am la : %d",d3);
 //	printf("\nSo ky tu so la : %d",d2);
-	printf("\nSo ky tu khac la : %d",n-d1-d3-d2); // Tim ky tu khac bang cach tru di d1,d3,d2
+	printf("\nSo ky tu khac la : %d",n-d1); // Tim ky tu khac bang cach tru di d1,d3,d2
 }
